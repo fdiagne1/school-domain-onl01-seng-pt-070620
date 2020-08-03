7aa2146357b455255b264ea3ccd1683a524a5e98
Fatou Diagne <f.m.diagne1@gmail.com>
2020-08-03 19:29:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,8 @@ class School
   end
   
   def add_student(name, grade)
+    @roster[grade] ||= [] # if what is on the left hand side of the or equals(||=) doen't exist, create it and set it equal to what is on the right side. If what is on the left hside does exist do nothing.
+    
     @roster[grade] << name
   end
   
